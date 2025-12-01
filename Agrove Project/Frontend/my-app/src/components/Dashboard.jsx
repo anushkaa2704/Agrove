2fbc56f19a0574bf16044ebd4c74dd66a5c6cfd3
anushka <anushkaa2704@gmail.com>
2025-12-01 23:31:21 +0530

    Dashboard code updated

--- Agrove Project/Frontend/my-app/src/components/Dashboard.jsx
+++ Agrove Project/Frontend/my-app/src/components/Dashboard.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Navbar from './Navbar';
 
-const Dashboard = () => {
+const Dashboard = () => {  
   const navigate = useNavigate();
   const [user, setUser] = useState({ name: "Farmer" });
 
